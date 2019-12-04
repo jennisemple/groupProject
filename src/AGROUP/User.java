bd583f1da2ffcbd5931d757be447e4f0a42cb88a
Jenni <Semple-j8@ulster.ac.uk>
2019-12-04 14:08:28 +0000

    amended user class

--- src/AGROUP/User.java
+++ src/AGROUP/User.java
@@ -7,7 +7,7 @@ public class User {
     private static int numOfprojectors;
     private static int numOfwhiteboards;
     private static int numOfDisabledAccess;
-    private int numberOfPeople;
+    private int numberOfSeats;
     private boolean projector;
     private boolean whiteboard;
     private boolean disabledAccess;
@@ -16,23 +16,52 @@ public class User {
         numberOfRooms++;
     }//Default constructor
 
-    public User(int roomNo, int noOfProjectors, int noOfWhiteboards, int noOfDisabledAccess, int noOfPeople) {
-        roomNumber = roomNo;
-        numOfprojectors = noOfProjectors;
-        numOfwhiteboards = noOfWhiteboards;
-        numOfDisabledAccess = noOfDisabledAccess;
-        numberOfPeople = noOfPeople;
+    public User(int inputroomNo, int inputnoOfProjectors, int inputnoOfWhiteboards, int inputnoOfDisabledAccess, int inputnoOfSeats) {
+        roomNumber = inputroomNo;
+        numOfprojectors = inputnoOfProjectors;
+        numOfwhiteboards = inputnoOfWhiteboards;
+        numOfDisabledAccess = inputnoOfDisabledAccess;
+        numberOfSeats = inputnoOfSeats;
         numberOfRooms++;
     }//Alternative Constructor
 
-    //public void getNumberOfRooms()
+    public static int getNumberOfRooms() {
+        return numberOfRooms;
+    }//getNumberOfRooms
+    public int getRoomNumber() {
+        return roomNumber;
+    }//getRoomNumber
+    public int getNumOfProjectors() {
+        return numOfprojectors;
+    }//getNumOfProjectors
+    public int getNumOfwhiteboards() {
+        return numOfwhiteboards;
+    }//getNumOfwhiteboards
+    public static int getNumOfDisabledAccess() {
+        return numOfDisabledAccess;
+    }//getNumOfDisabledAccess
 
-
-
-
-
-
-
-
-
+    public static void setRoomNumber(int roomNumber) {
+        User.roomNumber = roomNumber;
+    }//
+    public static void setNumOfprojectors(int numOfprojectors) {
+        User.numOfprojectors = numOfprojectors;
+    }
+    public static void setNumOfwhiteboards(int numOfwhiteboards) {
+        User.numOfwhiteboards = numOfwhiteboards;
+    }
+    public static void setNumOfDisabledAccess(int numOfDisabledAccess) {
+        User.numOfDisabledAccess = numOfDisabledAccess;
+    }
+    public boolean isDisabledAccess() {
+        return disabledAccess;
+    }
+    public boolean isProjector() {
+        return projector;
+    }
+    public boolean isWhiteboard() {
+        return whiteboard;
+    }
 }
+
+
